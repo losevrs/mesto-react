7d95baff17f791edc23d42997ae144d67fda6134
Лосев Родион Станиславович <@Lizin.local>
2020-08-28 15:18:20 +0300

    feat: Попапы до вынесения общих компонентов. Пункт 5.

--- src/components/App.js
+++ src/components/App.js
@@ -10,49 +10,49 @@ export default class App extends React.Component {
         <Header />
         <Main />
         <Footer />
-  
+
         <section className="popup popup_profileedit">
           <form className="popup__container" action="#" method="POST" name="popupform" novalidate>
             <h2 className="popup__title">Редактировать профиль</h2>
-  
+
             <input className="popup__input popup__input_name" type="text" name="name" placeholder="Имя" required minlength="2" maxlength="40" />
             <span id="popup__input_name_error" class="popup__input_type_error"></span>
-  
+
             <input className="popup__input popup__input_about" type="text" name="about" placeholder="Описание" required minlength="2" maxlength="200" />
             <span id="popup__input_about_error" class="popup__input_type_error"></span>
-  
+
             <button className="popup__submit" type="submit">Сохранить</button>
             <button className="popup__reset" type="button"></button>
           </form>
         </section>
-  
+
         <section className="popup popup_newplace">
           <form className="popup__container" action="#" method="POST" name="popupplace" novalidate>
             <h2 className="popup__title">Новое место</h2>
-  
+
             <input className="popup__input popup__input_photoname" type="text" name="photoName" placeholder="Название" required minlength="1" maxlength="30" />
             <span id="popup__input_photoName_error" className="popup__input_type_error"></span>
-  
+
             <input className="popup__input popup__input_photourl" type="url" name="photoUrl" placeholder="Ссылка на картинку" required />
             <span id="popup__input_photoUrl_error" className="popup__input_type_error"></span>
-  
+
             <button className="popup__submit" type="submit">Сохранить</button>
             <button className="popup__reset" type="button"></button>
           </form>
         </section>
-  
+
         <section className="popup popup_newavatar">
           <form className="popup__container popup_avaeditor" action="#" method="POST" name="popupavatar" novalidate>
             <h2 className="popup__title">Обновить аватар</h2>
-  
+
             <input className="popup__input popup__input_avatar" type="url" name="avatar" placeholder="Ссылка на картинку" required />
             <span id="popup__input_avatar_error" class="popup__input_type_error"></span>
-  
+
             <button className="popup__submit" type="submit">Сохранить</button>
             <button className="popup__reset" type="button"></button>
           </form>
         </section>
-  
+
         <section className="popup popup_confirm" action="#" method="POST" name="popupconfirm" novalidate>
           <form className="popup__container popup__question">
             <h2 className="popup__text">Вы уверены?</h2>
@@ -60,7 +60,7 @@ export default class App extends React.Component {
             <button className="popup__reset" type="button"></button>
           </form>
         </section>
-  
+
         <section className="popup popup_view">
           <div className="popup__photoview">
             <img className="popup__image" alt="Просмотр фото" src="./images/placesphotos/not.png" />
@@ -68,7 +68,7 @@ export default class App extends React.Component {
             <button className="popup__reset" type="button"></button>
           </div>
         </section>
-  
+
         <template id="card">
           <div className="photocard">
             <img className="photocard__viewport" src="#" alt="Фото места" />
@@ -82,7 +82,7 @@ export default class App extends React.Component {
             </div>
           </div>
         </template>
-  
+
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,41 @@ import React from 'react';
 import avatar from '../images/profile/member.png';
 
 export default class Main extends React.Component {
+
+  handleEditAvatarClick = () => {
+    const popupElement = document.querySelector('.popup_newavatar');
+      if (!popupElement.classList.contains('popup_opened')) {
+        popupElement.classList.add('popup_opened');
+      }
+  };
+
+  handleEditProfileClick = () => {
+    const popupElement = document.querySelector('.popup_profileedit');
+      if (!popupElement.classList.contains('popup_opened')) {
+        popupElement.classList.add('popup_opened');
+      }
+  };
+
+  handleAddPlaceClick = () => {
+    const popupElement = document.querySelector('.popup_newplace');
+      if (!popupElement.classList.contains('popup_opened')) {
+        popupElement.classList.add('popup_opened');
+      }
+  };
+
   render() {
     return (
       <main className="main">
         <section className="profile">
-          <img className="profile__avatar" src={avatar} alt="Аватар пользователя" />
+          <img className="profile__avatar" src={avatar} alt="Аватар пользователя" onClick={this.handleEditAvatarClick}/>
           <div className="profile__info">
             <div className="profile__title">
               <h1 className="profile__name">_</h1>
-              <button className="profile__editbutton" type="button"></button>
+              <button className="profile__editbutton" onClick={this.handleEditProfileClick} type="button"></button>
             </div>
             <p className="profile__about"></p>
           </div>
-          <button className="profile__addbutton" type="button"></button>
+          <button className="profile__addbutton" onClick={this.handleAddPlaceClick} type="button"></button>
         </section>
 
         <section className="placesphotos">
