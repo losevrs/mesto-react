ad01615524ad9a9f600e3625f73c473f99e3e095
Лосев Родион Станиславович <@Lizin.local>
2020-09-03 15:38:29 +0300

    feat: сделан просмотр карточки. 10ая в первом приближении готова.

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 export default class ImagePopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.popupOverlay = React.createRef();
+  }
+
+  closeOnOverlay = (event) => {
+    if (event.target === this.popupOverlay.current) {
+      this.props.onClose();
+    }
+  }
+
   render() {
     console.log(this.props);
     return (
-      <section className={`popup popup_view ${this.props.isOpened && "popup_opened"}`}>
+      <section className={`popup popup_view ${this.props.isOpened && "popup_opened"}`}
+        onClick={this.closeOnOverlay}
+        ref={this.popupOverlay}>
         <div className="popup__photoview">
           <img className="popup__image" alt="Просмотр фото" src={this.props.card.link} />
           <h2 className="popup__imagetitle">{this.props.card.name}</h2>
