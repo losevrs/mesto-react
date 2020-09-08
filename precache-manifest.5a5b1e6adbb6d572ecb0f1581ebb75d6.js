8cbbfed5169c1f61845a812672468de8d459ecd5
Лосев Родион Станиславович <@Lizin.local>
2020-09-08 13:28:19 +0300

    Updates

---
+++ precache-manifest.5a5b1e6adbb6d572ecb0f1581ebb75d6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798f862731cdc525c4a9f68ca83a4b99",
+    "url": "/losevrs/mesto-reactindex.html"
+  },
+  {
+    "revision": "e8612340291647a0d9de",
+    "url": "/losevrs/mesto-reactstatic/css/main.8e005689.chunk.css"
+  },
+  {
+    "revision": "2298e97e7bf016b5324f",
+    "url": "/losevrs/mesto-reactstatic/js/2.d6310a79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/losevrs/mesto-reactstatic/js/2.d6310a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8612340291647a0d9de",
+    "url": "/losevrs/mesto-reactstatic/js/main.9aa6f838.chunk.js"
+  },
+  {
+    "revision": "d5b3d34505b03a6e57c4",
+    "url": "/losevrs/mesto-reactstatic/js/runtime-main.188f3e49.js"
+  },
+  {
+    "revision": "c7df05485663b2b3ec3b99326f0f5ea2",
+    "url": "/losevrs/mesto-reactstatic/media/CloseIcon.c7df0548.svg"
+  },
+  {
+    "revision": "015fff8625e5b40a33a978d4a4a502ab",
+    "url": "/losevrs/mesto-reactstatic/media/CloseIconSm.015fff86.svg"
+  },
+  {
+    "revision": "ac9f293799cc1ea88155b5abd267b418",
+    "url": "/losevrs/mesto-reactstatic/media/EditButton18.ac9f2937.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "/losevrs/mesto-reactstatic/media/EditButton24.b05083e5.svg"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/losevrs/mesto-reactstatic/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/losevrs/mesto-reactstatic/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "8ef77a031c5b96e53f2eeeb009232181",
+    "url": "/losevrs/mesto-reactstatic/media/Inter-BoldItalic.8ef77a03.woff2"
+  },
+  {
+    "revision": "9528384cf7aebfa81a198626cb05f5e8",
+    "url": "/losevrs/mesto-reactstatic/media/Inter-Italic.9528384c.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/losevrs/mesto-reactstatic/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/losevrs/mesto-reactstatic/media/Trash.efa6fb04.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/losevrs/mesto-reactstatic/media/avaEdit.d476a8b8.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/losevrs/mesto-reactstatic/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "a61661316f636da84e813d4eab10a242",
+    "url": "/losevrs/mesto-reactstatic/media/onerror.a6166131.jpg"
+  },
+  {
+    "revision": "e0270e6710268c67a26289e12877ccbb",
+    "url": "/losevrs/mesto-reactstatic/media/plus16.e0270e67.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/losevrs/mesto-reactstatic/media/plus22.b2fce4ac.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.390355de2f889c26265f15d9175141c0.js"
+  "/losevrs/mesto-reactprecache-manifest.5a5b1e6adbb6d572ecb0f1581ebb75d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/losevrs/mesto-reactindex.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
